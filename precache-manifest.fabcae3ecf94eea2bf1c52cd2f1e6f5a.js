11a0db2c99fddf736089da663198763aed1698bc
Danylo <sukhiy37078@gmail.com>
2020-09-24 16:27:21 +0300

    Updates

---
+++ precache-manifest.fabcae3ecf94eea2bf1c52cd2f1e6f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6382730252d1a080f703c17bf8c57ba5",
+    "url": "/react-3/index.html"
+  },
+  {
+    "revision": "a8437277d5ff178b6aeb",
+    "url": "/react-3/static/css/main.2b5c28bc.chunk.css"
+  },
+  {
+    "revision": "9147c80d7272f004b1b9",
+    "url": "/react-3/static/js/2.552ca60b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-3/static/js/2.552ca60b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8437277d5ff178b6aeb",
+    "url": "/react-3/static/js/main.5ae6036e.chunk.js"
+  },
+  {
+    "revision": "a4408631909d468e5df9",
+    "url": "/react-3/static/js/runtime-main.94e8cba0.js"
+  }
+]);
